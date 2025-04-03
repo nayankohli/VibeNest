d46f5a0b4825cec375186470504a5f7cff91edac
Nayan Kohli <nayankohli7@gmail.com>
2025-04-03 15:47:04 +0530

    cors chnaged

--- backend/index.js
+++ backend/index.js
@@ -189,7 +189,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://vibenest-lm4w.onrender.com"],
+    origin: ["http://localhost:3000", "https://vibenest-lm4w.onrender.com","https://vibe-nest-dyyi.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -251,6 +251,7 @@ app.use("/api/chat", chatRoutes);
 const { notFound,errorHandler } = require('./middlewares/errorMiddleware.js');
 app.use(notFound);
 app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
